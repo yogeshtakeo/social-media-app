2a2b8ba58d1000b1b177c4ac1d2024405ace280a
yogeshtakeo <yogesh@takeo.ai>
2023-04-25 16:56:49 -0400

    Added seeding mechanism route

--- backend/routes/Posts.js
+++ backend/routes/Posts.js
@@ -1,8 +1,10 @@
 import { Router } from "express";
 import { GetAllPosts } from "../Controller/PostController.js";
+import { add_data } from "../seeders/Posts.js";
 
 const PostRouter = Router();
 
 PostRouter.get("/", GetAllPosts);
+PostRouter.get("/seed/posts", add_data);
 
 export default PostRouter;
