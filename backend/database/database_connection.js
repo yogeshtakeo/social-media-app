92cb83081132a44369052b5e310e3484d4f00994
Sanjay <sanjayluitel2@gmail.com>
2023-04-26 14:30:28 -0400

    initial

--- backend/database/database_connection.js
+++ backend/database/database_connection.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 const connectDatabase = async () => {
   const connection = await mongoose.connect(
-    `mongodb+srv://bhattaraiyogesh007:${process.env.DATABASE_PASSWORD}@${process.env.DATABASE_CLUSTER_NAME}.mongodb.net/?retryWrites=true&w=majority`
+    `mongodb+srv://${process.env.DATABASE_USERNAME}:${process.env.DATABASE_PASSWORD}@${process.env.DATABASE_CLUSTER_NAME}.mongodb.net/?retryWrites=true&w=majority`
   );
   if (connection) {
     console.log("database connected");
