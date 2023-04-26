5bff4ad386ff375d17660ec80a8ba8192a756bf4
yogeshtakeo <yogesh@takeo.ai>
2023-04-26 12:37:40 -0400

    Login and registeration created with token verification

--- backend/index.js
+++ backend/index.js
@@ -1,9 +1,10 @@
 import bodyParser from "body-parser";
 import express from "express";
-import PostRouter from "./routes/Posts.js";
+import PostRouter from "./routes/PostsRouter.js";
 import connectDatabase from "./database/database_connection.js";
 import dotenv from "dotenv";
 import cors from "cors";
+import UserRouter from "./routes/UsersRouter.js";
 
 dotenv.config();
 const app = express();
@@ -14,6 +15,7 @@ app.use(bodyParser.json());
 app.use(cors());
 
 app.use("/posts", PostRouter);
+app.use("/users", UserRouter);
 
 app.listen(8000, () => {
   console.log("Server Listenning");
