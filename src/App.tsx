e12be5da2c222da5717bf37a45800797858dbd48
yogeshtakeo <yogesh@takeo.ai>
2023-04-27 14:03:15 -0400

    Added Routing capability

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,19 @@
+import { Route, Routes } from "react-router-dom";
 import "./App.css";
+import Home from "./pages/Home";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
 
 function App(): JSX.Element {
-  return <></>;
+  return (
+    <>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+      </Routes>
+    </>
+  );
 }
 
 export default App;
