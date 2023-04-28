b95ae9cdd4e7f43e0b8b2434bd62557ab0f1f395
yogeshtakeo <yogesh@takeo.ai>
2023-04-28 14:35:26 -0400

    Updated Login and registration with backend

--- src/component/LoginComponent.tsx
+++ src/component/LoginComponent.tsx
@@ -1,31 +1,61 @@
-import { TextInput, Label, Checkbox, Button } from "flowbite-react";
+import { TextInput, Label, Checkbox, Button, Spinner } from "flowbite-react";
+import { ChangeEvent, ChangeEventHandler, useEffect, useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { postLogin } from "../store/slices/UserSlice";
 
 const LoginComponent = (): JSX.Element => {
+  const loading = useSelector((state) => state.users.loading);
+  const dispatch = useDispatch();
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    dispatch(postLogin({ username, password }));
+  };
+
   return (
-    <form className="flex flex-col gap-4">
+    <form className="flex flex-col gap-4" onSubmit={handleSubmit}>
       <h1 className="text-2xl text-center">Login</h1>
       <div>
         <div className="mb-2 block">
-          <Label htmlFor="email1" value="Your email" />
+          <Label htmlFor="username" value="Your username" />
         </div>
         <TextInput
-          id="email1"
-          type="email"
-          placeholder="name@flowbite.com"
+          id="username"
+          type="text"
+          placeholder="Username"
           required={true}
+          onChange={(event: React.ChangeEvent<HTMLInputElement>) =>
+            setUsername(event.target.value)
+          }
         />
       </div>
       <div>
         <div className="mb-2 block">
-          <Label htmlFor="password1" value="Your password" />
+          <Label htmlFor="password" value="Your password" />
         </div>
-        <TextInput id="password1" type="password" required={true} />
+        <TextInput
+          id="password"
+          type="password"
+          required={true}
+          onChange={(event: React.ChangeEvent<HTMLInputElement>) =>
+            setPassword(event.target.value)
+          }
+        />
       </div>
       <div className="flex items-center gap-2">
         <Checkbox id="remember" />
         <Label htmlFor="remember">Remember me</Label>
       </div>
-      <Button type="submit">Submit</Button>
+      {loading ? (
+        <Button>
+          <Spinner aria-label="Spinner button example" />
+          <span className="pl-3">Loading...</span>
+        </Button>
+      ) : (
+        <Button type="submit">Submit</Button>
+      )}
       <Button outline> Create a new account</Button>
     </form>
   );
