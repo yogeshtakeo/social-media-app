34cef523e62cf544afc710dda2b44573cff3c50d
yogeshtakeo <yogesh@takeo.ai>
2023-05-01 14:38:02 -0400

    Posts fetch Completed

--- src/store/reducers/index.ts
+++ src/store/reducers/index.ts
@@ -1,7 +1,9 @@
+import PostSlice from "../slices/PostSlice";
 import UserSlice from "../slices/UserSlice";
 
 const reducer = {
   users: UserSlice.reducer,
+  posts: PostSlice.reducer,
 };
 
 export default reducer;
