27c69b9cb881400a1bfb05bf2f6729e2594e2196
Sabin Sapkota <sabinsapkota4123@gmail.com>
2023-05-01 23:14:06 -0500

    Added Routing capablity

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,19 @@
+import { Route, Routes } from "react-router-dom";
 import "./App.css";
-import NavBarComponent from "./component/NavbarComponent";
-import SidebarComponent from "./component/SidebarComponent";
-import PostComponent from "./component/PostComponent";
-import LoginComponent from "./component/LoginComponent";
-import RegisterComponent from "./component/RegisterComponent";
-import Register from "./pages/Register";
-import Login from "./pages/Login";
 import Home from "./pages/Home";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
 
 function App(): JSX.Element {
-  return <Register />;
-  return <Login />;
-  return <Home />;
+  return (
+    <>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+      </Routes>
+    </>
+  );
 }
 
 export default App;
