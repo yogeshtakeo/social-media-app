e7839e814053acebc66ec36a139f0397e622ff84
sushilkhanal <khanal.sushil47@gmail.com>
2023-05-03 12:22:31 -0500

    env updated

--- backend/seeders/Posts.js
+++ backend/seeders/Posts.js
@@ -6,7 +6,7 @@ const PostsCreator = (count) => ({
   description: "This is the post for the Title " + count,
   time: new Date().toString(),
   like: count,
-  postedBy: new mongoose.Types.ObjectId("6449531df44633c1aba0ee21"),
+  postedBy: new mongoose.Types.ObjectId("6451499617ea4d4ad78970d6"),
 });
 
 export const add_data = (req, res) => {
